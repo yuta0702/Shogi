4e88500ef34445beb66e69571bb8252ea5e1e3af
Yuta Yamauchi <yuuta_y-12a@stu.kbc.ac.jp>
2013-06-19 15:08:55 +0900

    Initial commit

---
+++ src/PlayField.java
@@ -0,0 +1,43 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+
+public class PlayField extends JFrame implements MouseListener{
+	
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		
+		PlayField playfield = new PlayField();
+		playfield.setBounds(300, 50, 600, 600);
+		playfield.setVisible(true);
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		
+	}
+
+}
